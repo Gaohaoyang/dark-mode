dce9d9c5ce7418653bdea98540c2a536b70db582
chuanshi <ghy118432@alibaba-inc.com>
2020-06-28 17:48:12 +0800

    u

---
+++ background.js
@@ -0,0 +1,5 @@
+console.log('------------------------------------');
+console.log('hi');
+// 动态执行JS代码
+// chrome.tabs.executeScript(tabId, {code: 'document.body.style.backgroundColor="red"'});
+console.log('------------------------------------');
---
+++ content.js
@@ -0,0 +1,5 @@
+console.log('----dark mode----------------------')
+// if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+//   console.log('浏览器支持dark模式!(^_^)');
+// }
+
